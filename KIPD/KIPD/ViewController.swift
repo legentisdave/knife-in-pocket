18d34d661a38c981807d5848f8eb2516b67cf2f8
David Elliman <david.elliman@montvieux.com>
2019-03-15 17:44:24 +0000

    added xcode project

---
+++ KIPD/KIPD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KIPD
+//
+//  Created by David Elliman on 15/03/2019.
+//  Copyright © 2019 Legentis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
